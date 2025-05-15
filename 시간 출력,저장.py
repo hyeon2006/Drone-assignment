1734338b04fd194201cd9e9dd85a1245fdde3b80
Hyeon2 <97961602+hyeon2006@users.noreply.github.com>
2025-05-15 14:36:43 +0900

    update

---
+++ 시간 출력,저장.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from time import sleep
+
+# 현재 시간 가져오기
+st = datetime.now()
+print("시작 시간:",st)
+
+print("5초간 정지")
+sleep(5)
+
+# 현재 시간 가져오기
+et = datetime.now()
+print("종료 시간:",et)
+
+rt = et - st
+
+print("실행 시간:",rt)
+
+f = open("D:\python\드론 과제 생성물\Time Print.txt", "w")
+
+f.write("start_time : " + str(st) + "\n" + "end_time : " + str(et) + "\n" + "run_time : " + str(rt))
+
+f.close()

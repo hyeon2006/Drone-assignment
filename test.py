3bc725bcb3f86e80af6af192df1590c298a30126
Hyeon2 <97961602+hyeon2006@users.noreply.github.com>
2025-05-16 15:04:47 +0900

    Update test.py

--- test.py
+++ test.py
@@ -81,6 +81,30 @@ def spinR(power, duration):
     drone.set_yaw(0)
 
 
+def right(power, duration):
+    """
+    오른쪽방향으로 회전
+
+    :param power: 비행속도
+    :param duration: 비행시간
+    """
+    drone.set_roll(power)
+    drone.move(duration)
+    drone.set_roll(0)
+
+
+def left(power, duration):
+    """
+    왼쪽방향으로 회전
+
+    :param power: 비행속도
+    :param duration: 비행시간
+    """
+    drone.set_roll(-power)
+    drone.move(duration)
+    drone.set_roll(0)
+
+
 def buzz():
     drone.drone_buzzer(Note.C4, 500)
     drone.drone_buzzer(Note.D4, 500)
@@ -93,30 +117,36 @@ drone = Drone()
 
 # 드론 페어링
 drone.pair()
+drone.drone_LED_off()
 format()
 # buzz()
 drone.takeoff()
 
-up(40, 1)
-forward(28, 4.5)
-down(30, 3.75)
-forward(35, 2)
-up(40, 5)
-forward(30, 3.5)
-down(30, 6)
-forward(30, 2.5)
+up(40, 2)
+forward(28, 4.75)
+down(30, 2.4)
+forward(35, 2.5)
+up(40, 3.75)
+forward(30, 3.25)
+down(30, 3.25)
+forward(30, 2.75)
 up(30, 4)
+spinL(20, 3)
+# buzz()
+forward(30, 4)
+left(30, 2)
+backward(30, 2)
+right(30, 2)
+forward(30, 4)
 spinL(25, 3)
 # buzz()
-forward(30, 2)
-spinL(25, 4)
-forward(30, 4)
-spinL(30, 10)
-# buzz()
+
 drone.hover(5)
 # sort.sorting()
 
+forward(30, 6)
+
 
 drone.land()
-
+# buzz()
 drone.close()
